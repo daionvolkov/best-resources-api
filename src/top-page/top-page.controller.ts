d6e9b8eb32e2a8dfaa8e73dc9d7c756a0c1d0167
daionvolkov <daionvolkov@gmail.com>
2024-04-04 17:53:27 +0300

    commit 04.04.2024

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,16 +1,21 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('top-page')
 export class TopPageController {
+
+  constructor(private readonly configService: ConfigService) { }
 
   @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>) { // Omit<ProductModel, '_id'> исключает _id из переданной модели
   }
 
   @Get(':id')
-  async get(@Param('id') is: string) { }
+  async get(@Param('id') is: string) {
+    this.configService.get('TEST');
+  }
 
   @Delete(':id')
   async delete(@Param('id') is: string) { }
