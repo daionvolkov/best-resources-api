f9596e55597dfe2f7c5ce3ce8b5cd912a0e4814f
daionvolkov <101796073+daionvolkov@users.noreply.github.com>
2024-04-05 20:48:25 +0300

    add db connection 05.04.2024

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { TopPageModule } from './top-page/top-page.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, ProductModule, ReviewModule, TopPageModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/test'), 
+    AuthModule,
+    ProductModule,
+    ReviewModule,
+    //TopPageModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
